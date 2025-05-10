6240205d67b94bb0975fb7048707426ee5f3fd12
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-10 13:59:40 +0100

    more tests to check the withdraws functionalities

--- foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -88,4 +88,33 @@ contract FundMeTest is Test {
             endingOwnerBalance
         );
     }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        //arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank
+            //vm.deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+            //fund me
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //act
+        vm.prank(fundMe.getOwner()); // prank to owner
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        //assert -> check if the owner balance has changed
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
