c95c41aef121ec3fea9605fb214fe47599dc729c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-11 17:58:09 +0100

    testing withdraw cheaper function

--- foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -118,4 +118,33 @@ contract FundMeTest is Test {
                 fundMe.getOwner().balance
         );
     }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        //arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank
+            //vm.deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+            //fund me
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //act
+        vm.prank(fundMe.getOwner()); // prank to owner
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        //assert -> check if the owner balance has changed
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
