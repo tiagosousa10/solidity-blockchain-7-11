f3507077fcb9e3837ea5b6554808fac2860b6d07
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-02 11:51:40 +0100

    Fund Subscription contract added, first steps

--- foundry-f23/foundry-smart-contract-lottery-f23/script/Interactions.s.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/script/Interactions.s.sol
@@ -28,3 +28,17 @@ contract CreateSubscription is Script {
         return createSubscriptionUsingConfig();
     }
 }
+
+contract FundSubscription is Script {
+    uint96 public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        (, , address vrfCoordinator, , uint64 subId, ) = helperConfig
+            .activeNetworkConfig();
+    }
+
+    function run() external {
+        fundSubscriptionUsingConfig();
+    }
+}
