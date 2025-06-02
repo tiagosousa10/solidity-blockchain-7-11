9756ff5fabd4bd8273710e1803995b435a917c36
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-02 11:57:36 +0100

    LinkToken added to the test mocks

--- foundry-f23/foundry-smart-contract-lottery-f23/script/HelperConfig.s.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/script/HelperConfig.s.sol
@@ -12,6 +12,7 @@ contract HelperConfig is Script {
         bytes32 gasLane;
         uint64 subscriptionId;
         uint32 callbackGasLimit;
+        address link; // Only for Sepolia, not used in Anvil
     }
 
     NetworkConfig public activeNetworkConfig;
@@ -32,7 +33,8 @@ contract HelperConfig is Script {
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 subscriptionId: 0, // update with your subscription ID
-                callbackGasLimit: 500000 // 500,000 gas
+                callbackGasLimit: 500000, // 500,000 gas
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
             });
     }
 
