550d2ca11f056fdda6283ccbc25d53ad71d8cd99
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-02 12:32:47 +0100

    deployRaffle script updated

--- foundry-f23/foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -26,6 +26,14 @@ contract DeployRaffle is Script {
             subscriptionId = createSubscription.createSubscription(
                 vrfCoordinator
             );
+
+            //fund the subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                link
+            );
         }
 
         vm.startBroadcast();
@@ -40,6 +48,13 @@ contract DeployRaffle is Script {
         );
 
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinator,
+            subscriptionId
+        );
         return (raffle, helperConfig);
     }
 }
