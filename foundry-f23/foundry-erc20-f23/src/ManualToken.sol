06837b5944fd120285e68f4ed88bb5a039d92e38
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-14 20:17:48 +0100

    writting balanceOf and transfer functionalities

--- foundry-f23/foundry-erc20-f23/src/ManualToken.sol
+++ foundry-f23/foundry-erc20-f23/src/ManualToken.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.18;
 
 contract ManualToken {
+   mapping (address => uint256) private s_balances;
+
    function name() public pure returns (string memory) {
       return "ManualToken";
    }
@@ -12,5 +14,16 @@ contract ManualToken {
 
    function decimals() public pure returns (uint8) {
       return 18;
+   }
+
+   function balanceOf(address _owner) public view returns (uint256) {
+      return s_balances[_owner]; // -> 
+   }
+
+   function transfer(address _to, uint256 _amount) public {
+      uint256 previousBalances = balanceOf(msg.sender) +  balanceOf(_to);
+      s_balances[msg.sender] -= _amount;
+      s_balances[_to] += _amount;
+      require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
    }
 }
